9873b2f5aa0fa38587c827aef0be32fe51cf0a4b
Q-A-F-M <fabian.hoenig@gmail.com>
2025-01-16 10:45:22 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+import { ObsidianAssistant } from './ObsidianAssistant.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(ObsidianAssistant));
